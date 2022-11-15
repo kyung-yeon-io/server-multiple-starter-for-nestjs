4c8646fbc24c0e70f571ec15641d9c3673e89d03
eric-byun <eric@washswat.com>
2022-11-15 12:08:37 +0900

    api tester 추가

---
+++ script/nginx-conf.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "
+#user  nobody;
+worker_processes  1;
+
+#error_log  logs/error.log;
+#error_log  logs/error.log  notice;
+#error_log  logs/error.log  info;
+
+#pid        logs/nginx.pid;
+
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    #log_format  main  ' -  []  '
+    #                  '   '
+    #                  ' ';
+
+    #access_log  logs/access.log  main;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    #keepalive_timeout  0;
+    keepalive_timeout  65;
+
+    #gzip  on;
+
+    server {
+        listen       3000;
+        server_name  localhost;
+
+        #charset koi8-r;
+
+        #access_log  logs/host.access.log  main;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+
+        # custom --->
+        $2
+        # <--- custom
+    }
+
+    include servers/*;
+}
+" > $1
